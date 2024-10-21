c0580d5f76588b484dbb01f02b2f957be8dd1382
rongzhang <rongzhang@pinterest.com>
2024-10-21 13:23:44 +0000

    change serialize to use markdown

--- querybook/tests/test_lib/test_github_integration/test_serializers.py
+++ querybook/tests/test_lib/test_github_integration/test_serializers.py
@@ -1,7 +1,10 @@
 import pytest
-import json
+import yaml
 from const.data_doc import DataCellType
-from lib.github_integration.serializers import serialize_datadoc, deserialize_datadoc
+from lib.github_integration.serializers import (
+    serialize_datadoc_to_markdown,
+    deserialize_datadoc_from_markdown,
+)
 from models.datadoc import DataCell, DataDoc
 
 
@@ -14,6 +17,7 @@ def mock_datadoc():
             context="SELECT * FROM table;",
             created_at="2023-01-01T00:00:00Z",
             updated_at="2023-01-01T00:00:00Z",
+            meta={"variables": [{"name": "var1", "value": "value1", "type": "string"}]},
         ),
         DataCell(
             id=2,
@@ -21,6 +25,7 @@ def mock_datadoc():
             context="This is a text cell.",
             created_at="2023-01-01T00:00:00Z",
             updated_at="2023-01-01T00:00:00Z",
+            meta={"variables": [{"name": "var2", "value": True, "type": "boolean"}]},
         ),
     ]
     datadoc = DataDoc(
@@ -34,35 +39,140 @@ def mock_datadoc():
         title="Test DataDoc",
         cells=cells,
     )
+    datadoc.meta = {"variables": [{"name": "doc_var", "value": 123, "type": "number"}]}
     return datadoc
 
 
-def test_serialize_datadoc(mock_datadoc):
-    serialized = serialize_datadoc(mock_datadoc)
-    datadoc_dict = mock_datadoc.to_dict(with_cells=True)
-    expected_serialized = json.dumps(datadoc_dict, indent=4, default=str)
-    assert serialized == expected_serialized
+class TestSerializers:
+    def test_serialize_datadoc_to_markdown(self, mock_datadoc):
+        serialized = serialize_datadoc_to_markdown(mock_datadoc)
+        expected_serialized = (
+            "---\n"
+            "archived: false\n"
+            "created_at: '2023-01-01T00:00:00Z'\n"
+            "environment_id: 1\n"
+            "id: 1\n"
+            "meta:\n"
+            "  variables:\n"
+            "  - name: doc_var\n"
+            "    type: number\n"
+            "    value: 123\n"
+            "owner_uid: user1\n"
+            "public: true\n"
+            "title: Test DataDoc\n"
+            "updated_at: '2023-01-01T00:00:00Z'\n"
+            "---\n\n"
+            "# Test DataDoc\n\n"
+            "---\n"
+            "id: 1\n"
+            "cell_type: query\n"
+            "created_at: '2023-01-01T00:00:00Z'\n"
+            "updated_at: '2023-01-01T00:00:00Z'\n"
+            "meta:\n"
+            "  variables:\n"
+            "  - name: var1\n"
+            "    type: string\n"
+            "    value: value1\n"
+            "---\n"
+            "```query\n"
+            "SELECT * FROM table;\n"
+            "```\n\n"
+            "---\n"
+            "id: 2\n"
+            "cell_type: text\n"
+            "created_at: '2023-01-01T00:00:00Z'\n"
+            "updated_at: '2023-01-01T00:00:00Z'\n"
+            "meta:\n"
+            "  variables:\n"
+            "  - name: var2\n"
+            "    type: boolean\n"
+            "    value: true\n"
+            "---\n"
+            "```text\n"
+            "This is a text cell.\n"
+            "```"
+        )
 
+        # Keys in dicts are not ordered, need to split and parse into yaml dict for ordering
+        serialized_parts = serialized.split("---\n")
+        expected_parts = expected_serialized.split("---\n")
 
-def test_deserialize_datadoc(mock_datadoc):
-    datadoc_dict = mock_datadoc.to_dict(with_cells=True)
-    json_content = json.dumps(datadoc_dict, indent=4, default=str)
-    deserialized = deserialize_datadoc(json_content)
+        # Parse metadata into dict
+        serialized_metadata = yaml.safe_load(serialized_parts[1].strip())
+        expected_metadata = yaml.safe_load(expected_parts[1].strip())
+        assert serialized_metadata == expected_metadata
 
-    assert deserialized.id == mock_datadoc.id
-    assert deserialized.environment_id == mock_datadoc.environment_id
-    assert deserialized.public == mock_datadoc.public
-    assert deserialized.archived == mock_datadoc.archived
-    assert deserialized.owner_uid == mock_datadoc.owner_uid
-    assert deserialized.created_at == mock_datadoc.created_at
-    assert deserialized.updated_at == mock_datadoc.updated_at
-    assert deserialized.title == mock_datadoc.title
-    assert deserialized.meta == mock_datadoc.meta
-    assert len(deserialized.cells) == len(mock_datadoc.cells)
-    for d_cell, m_cell in zip(deserialized.cells, mock_datadoc.cells):
-        assert d_cell.id == m_cell.id
-        assert d_cell.cell_type == m_cell.cell_type
-        assert d_cell.context == m_cell.context
-        assert d_cell.meta == m_cell.meta
-        assert d_cell.created_at == m_cell.created_at
-        assert d_cell.updated_at == m_cell.updated_at
+        # Parse content into dict
+        serialized_content_1 = yaml.safe_load(serialized_parts[3].strip())
+        expected_content_1 = yaml.safe_load(expected_parts[3].strip())
+        serialized_content_2 = yaml.safe_load(serialized_parts[5].strip())
+        expected_content_2 = yaml.safe_load(expected_parts[5].strip())
+        assert serialized_content_1 == expected_content_1
+        assert serialized_content_2 == expected_content_2
+
+    def test_deserialize_datadoc_from_markdown(self, mock_datadoc):
+        markdown_content = (
+            "---\n"
+            "archived: false\n"
+            "created_at: '2023-01-01T00:00:00Z'\n"
+            "environment_id: 1\n"
+            "id: 1\n"
+            "meta:\n"
+            "  variables:\n"
+            "  - name: doc_var\n"
+            "    type: number\n"
+            "    value: 123\n"
+            "owner_uid: user1\n"
+            "public: true\n"
+            "title: Test DataDoc\n"
+            "updated_at: '2023-01-01T00:00:00Z'\n"
+            "---\n\n"
+            "# Test DataDoc\n\n"
+            "---\n"
+            "created_at: '2023-01-01T00:00:00Z'\n"
+            "id: 1\n"
+            "meta:\n"
+            "  variables:\n"
+            "  - name: var1\n"
+            "    type: string\n"
+            "    value: value1\n"
+            "cell_type: query\n"
+            "updated_at: '2023-01-01T00:00:00Z'\n"
+            "---\n"
+            "```query\n"
+            "SELECT * FROM table;\n"
+            "```\n\n"
+            "---\n"
+            "created_at: '2023-01-01T00:00:00Z'\n"
+            "id: 2\n"
+            "meta:\n"
+            "  variables:\n"
+            "  - name: var2\n"
+            "    type: boolean\n"
+            "    value: true\n"
+            "cell_type: text\n"
+            "updated_at: '2023-01-01T00:00:00Z'\n"
+            "---\n"
+            "```text\n"
+            "This is a text cell.\n"
+            "```"
+        )
+        deserialized = deserialize_datadoc_from_markdown(markdown_content)
+
+        assert deserialized.id == mock_datadoc.id
+        assert deserialized.environment_id == mock_datadoc.environment_id
+        assert deserialized.public == mock_datadoc.public
+        assert deserialized.archived == mock_datadoc.archived
+        assert deserialized.owner_uid == mock_datadoc.owner_uid
+        assert deserialized.created_at == mock_datadoc.created_at
+        assert deserialized.updated_at == mock_datadoc.updated_at
+        assert deserialized.title == mock_datadoc.title
+        assert deserialized.meta == mock_datadoc.meta
+        assert len(deserialized.cells) == len(mock_datadoc.cells)
+        for d_cell, m_cell in zip(deserialized.cells, mock_datadoc.cells):
+            assert d_cell.id == m_cell.id
+            assert d_cell.cell_type == m_cell.cell_type
+            assert d_cell.context == m_cell.context
+            assert d_cell.meta == m_cell.meta
+            assert d_cell.created_at == m_cell.created_at
+            assert d_cell.updated_at == m_cell.updated_at
